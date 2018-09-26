16e1960a1e44eb46dfbc7b7fd7b39bb17f3d7810
ruirui.qu <ruirui.qu@qunar.com>
2018-09-26 18:30:32 +0800

    add

---
+++ exercises/hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# --------------------- Change Logs----------------------
+# @Author  : ruirui.qu Initial Created at 18/9/26
+# -------------------------------------------------------
+
+print "你好";
